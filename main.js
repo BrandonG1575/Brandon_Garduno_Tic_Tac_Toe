23e1002b6a39312c75ed84ba23706383dc50744d
BrandonG1575 <brandgar172@gmail.com>
2023-09-28 13:57:41 -0400

    Marked Js, Created Init and Render Function, and tested the arrays functinalitity in dev tools

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Your JS is linked up. Be the person you needed when you were little.')
+console.log('Your JS is linked up. Be the person you needed when you were little.');
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let board;
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+
+function init(){
+    board = ['','','','','','','','',''];
+    render();
+};
+init();
+
+function render(){
+    board.forEach(function(mark, index){
+        console.log(mark, index);
+    });
+};
+
+
