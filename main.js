db93998a1e662e1a0bb0796ec05dc14889bc17e5
BrandonG1575 <brandgar172@gmail.com>
2023-09-28 14:12:27 -0400

    Created HandleTurn Function

--- main.js
+++ main.js
@@ -2,20 +2,27 @@ console.log('Your JS is linked up. Be the person you needed when you were little
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let board;
+let turn = 'X';
 /*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click',handleTurn);
 /*----- functions -----*/
 
 function init(){
     board = ['','','','','','','','',''];
     render();
 };
-init();
 
 function render(){
     board.forEach(function(mark, index){
-        console.log(mark, index);
+        squares[index].textContent = mark;
     });
 };
 
-
+function handleTurn(event){
+    let idx = squares.findIndex(function(square){
+        return square == event.target;
+    }); 
+};
+init();
