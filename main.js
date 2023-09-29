770b303bfe77c272d4593109f26cbb8c8acbadca
BrandonG1575 <brandgar172@gmail.com>
2023-09-29 13:06:09 -0400

    Tie FUnctions and Win Functions Finalized

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 console.log('Your JS is linked up. Be the person you needed when you were little.');
 /*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X';
+let win;
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
 const messages = document.querySelector('h2');
@@ -19,7 +30,7 @@ function render(){
     board.forEach(function(mark, index){
         squares[index].textContent = mark;
     });
-    messages.textContent = `It's ${turn}'s turn!`;
+    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
 };
 
 function handleTurn(event){
@@ -36,7 +47,16 @@ function handleTurn(){
     });
     board[idx] = turn;
     turn = turn == 'X' ? 'O' : 'X';
+    win = getWinner();
     render();
+};
+
+function getWinner(){
+    let winner = null;
+    winningCombos.forEach(function(combo,index){
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+    });
+    return winner ? winner : board.includes('') ? null : 'T';
 };
 
 init();
